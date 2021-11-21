0349e033cd483c878f6b664e1859e7e125673a11
CimpeanAndreea <andreeacimpean.910@gmail.com>
2021-11-21 21:16:45 +0200

    Added infinite scroll

--- myApp/src/product/ProductList.tsx
+++ myApp/src/product/ProductList.tsx
@@ -5,11 +5,15 @@ import {
     IonFab,
     IonFabButton,
     IonHeader,
-    IonIcon, IonItem, IonList,
+    IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonList,
     IonLoading,
     IonPage,
+    IonSearchbar,
+    IonSelect,
+    IonSelectOption,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    useIonViewWillEnter
 } from "@ionic/react";
 import Product from "./Product";
 import { getLogger } from "../core";
@@ -19,13 +23,67 @@ import { useNetwork } from "../network/useNetwork";
 import './Product.css';
 import { AuthContext, AuthState } from "../auth";
 import { ProductContext } from "./ProductProvider";
+import { ProductProps } from "./ProductProps";
 
 const log = getLogger('ProductList');
+const offset = 2;
 
 const ProductList: React.FC<RouteComponentProps> = ({ history }) => {
+    
     const { products, fetching, fetchingError } = useContext(ProductContext); // returns the value of the context(see the context as a service)
     const { networkStatus } = useNetwork();
     const { logout } = useContext<AuthState>(AuthContext);
+
+    const [disableInfiniteScroll, setDisableInfiniteScroll] = useState<boolean>(false);
+    const [visibleProducts, setVisibleProducts] = useState<ProductProps[] | undefined>([]);
+    const [page, setPage] = useState(0);
+    const [filter, setFilter] = useState<string | undefined>(undefined);
+    const [search, setSearch] = useState<string>("");
+
+    const categories = ["All", "Food", "Electronics", "Books", "Clothes"];
+
+    useEffect(() => {
+        if(products?.length && products?.length > 0) {
+            setPage(offset);
+            fetchData();
+        }
+    }, [products])
+
+    useEffect(() => {
+        if(products && filter) {
+            if (filter === "All") {
+                setVisibleProducts(products);
+            }
+            else {
+                setVisibleProducts(products.filter(each => each.category === filter));
+            }
+        }
+    }, [filter]);
+
+    useEffect(() => {
+        if(search === "") {
+            setVisibleProducts(products);
+        }
+        if(products && search !== "") {
+            setVisibleProducts(products.filter(each => each.productName.startsWith(search)));
+        }
+    }, [search])
+
+    function fetchData() {
+        setVisibleProducts(products?.slice(0, page + offset));
+        setPage(page + offset);
+    }
+
+    async function searchNext($event: CustomEvent<void>) {
+        setTimeout(() => {
+            fetchData();
+            if (products && page > products?.length) {
+                setDisableInfiniteScroll(true);
+                setPage(products.length);
+            }
+            ($event.target as HTMLIonInfiniteScrollElement).complete();
+          }, 1000);
+    }
 
     const handleLogout = () => { 
         log('handleLogout...');
@@ -49,13 +107,42 @@ const ProductList: React.FC<RouteComponentProps> = ({ history }) => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
+                <IonItem>
+                    <IonSearchbar placeholder="Search by name" value={search} debounce={200} onIonChange={(e) => {
+                        setSearch(e.detail.value!);
+                    }}>
+                    </IonSearchbar>
+                </IonItem>
+                <IonItem>
+                    <IonSelect value={filter} placeholder="Filter by category" onIonChange={(e) => setFilter(e.detail.value)}>
+                        {categories.map((each) => (
+                            <IonSelectOption key={each} value={each}>
+                                {each}
+                            </IonSelectOption>
+                        ))}
+                    </IonSelect>
+                </IonItem>
                 <IonLoading isOpen={fetching} message="Fetching products" />
-                {products && (
+                {visibleProducts && (
                     <IonList>
-                        {products.map(({ _id, productName, price, quantity, category }) =>
+                        {Array.from(visibleProducts)
+                            .filter(each => {
+                                if (filter !== undefined && filter !== "All")
+                                    return each.category === filter && each._id !== undefined;
+                                return each._id !== undefined;
+                            })
+                            .map(({ _id, productName, price, quantity, category }) =>
                             <Product key={_id} _id={_id} productName={productName} price={price} quantity={quantity} category={category} onEdit={_id => history.push(`/product/${_id}`)} />)}
                     </IonList>
                 )}
+
+                <IonInfiniteScroll threshold="100px" disabled={disableInfiniteScroll} onIonInfinite={(e: CustomEvent<void>) => searchNext(e)}>
+                    <IonInfiniteScrollContent 
+                        loadingSpinner="bubbles"
+                        loadingText="Loading...">
+                    </IonInfiniteScrollContent>
+                </IonInfiniteScroll>
+
                 {fetchingError && (
                     <div>{fetchingError.message || 'Failed to fetch products'}</div>
                 )}
